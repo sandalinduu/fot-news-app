38f4cb7c1cbc30ef402149853aa13289f70d5817
sandalinduu <sandalindu@gmail.com>
2025-06-09 12:05:06 +0530

    first commit

---
+++ app/src/main/java/lk/cmb/app/SplashScreen.java
@@ -0,0 +1,26 @@
+package lk.cmb.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Delay and start MainActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DURATION);
+    }
+}
