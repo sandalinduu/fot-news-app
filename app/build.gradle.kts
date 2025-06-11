2b16a2deb9cd0f9562aa70f07e3771246f295910
sandalinduu <167954938+sandalinduu@users.noreply.github.com>
2025-06-11 15:38:29 +0530

    newsscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,19 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
+    implementation(libs.activity) // This should bring in EdgeToEdge.enable()
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Glide for image loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // ADD THIS LINE FOR EdgeToEdge, ViewCompat, Insets, WindowInsetsCompat
+    implementation("androidx.core:core-ktx:1.13.1") // Use the latest stable version
+    // You can check for latest at https://developer.android.com/jetpack/androidx/releases/core
 }
